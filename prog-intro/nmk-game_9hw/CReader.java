eea8d0980f6eb5879c5d036defcaa9f0cbc68200
Danila Semischastnov <desemy4@ya.ru>
2020-11-24 23:54:53 +0400

    n,m,k-game (HW-9)

---
+++ prog-intro/nmk-game_9hw/CReader.java
@@ -0,0 +1,41 @@
+package game;
+
+import java.util.Scanner;
+
+public class CReader extends Main {
+    public Scanner sc = new Scanner(System.in);
+
+    private boolean checkK;
+
+    int readInt(boolean check) {
+        String s = sc.next();
+        this.checkK = check;
+        while (!isNum(s)) {
+            System.out.println("Your input is incorrect, please try again");
+            s = sc.next();
+        }
+        return Integer.parseInt(s);
+    }
+
+    String readPlayer() {
+        String s = sc.next();
+        while (!s.equals("Human") && !s.equals("Sequential") && !s.equals("Random")) {
+            System.out.println("Player type is incorrect, please tyr again");
+            s = sc.next();
+        }
+        return s;
+    }
+
+    private boolean isNum(String s) {
+        int x;
+        try {
+            x = Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        if (this.checkK) {
+            return (x <= n && x <= m) && x > 0;
+        }
+        return (x > 0);
+    }
+}
